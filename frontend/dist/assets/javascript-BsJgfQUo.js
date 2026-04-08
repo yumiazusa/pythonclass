638743dbcdf92ba837ab5a4ec4fb58390168b310
“yumiazusa” <yumiazusa@hotmail.com>
2026-04-08 16:49:39 +0800

    feat: guided template dynamic fields + password + custom import toggle + pymysql allowlist

--- frontend/dist/assets/javascript-BsJgfQUo.js
+++ frontend/dist/assets/javascript-BsJgfQUo.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-CaoLzOT9.js";import"./index-vWlllVnD.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-DN3xv-sa.js";import"./index-h3O_Rszd.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
